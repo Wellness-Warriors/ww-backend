768095d8e8f32b73cc0cdf982856a99fe8a0339b
antoni <lorenzo.antonio.090@gmail.com>
2021-05-17 23:11:02 -0700

    added delete method

--- server.js
+++ server.js
@@ -68,6 +68,25 @@ app.post('/entry', (req, res) => {
   });
 });
 
+app.delete('/entry/:id', (req,res)=>{
+  const id = req.params.id;
+  const email = req.query.user;
+
+  User.find({email: email},(err,results)=>{
+    let user = results[0];
+    console.log('MyUSER',user);
+    user.entry = user.entry.filter(entry => {
+      console.log('ENTRY',entry);
+      `${entry._id} !== ${id}`;
+    });
+
+    user.save().then((usersData)=> {
+      console.log('usersData',usersData);
+      res.status(200).send(usersData);
+    });
+  });
+});
+
 app.get('/', (req, res) => {
   //test server
   res.status(200).send('hello from root');
@@ -76,15 +95,3 @@ app.get('/', (req, res) => {
 app.get('/zen', getZen);
 
 app.listen(PORT, () => console.log(`server listening on port: ${PORT}`));
-
-// if (databaseResults.length < 1) {
-//   res.status(400).send('user does not exist');
-// } else {
-//   let user = new User({
-//     email: req.body.email,
-//     entry: [{
-//       date: req.body,
-//       emotion: 'fierce',
-//       notes: 'fierce notes'
-//     }]
-//   });
